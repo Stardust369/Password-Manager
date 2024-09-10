be8ef24d7414fb4f85b9c2fb7f7b1c07de957806
unknown <satyamjhadar@gmail.com>
2024-09-10 23:32:55 +0530

    started the project

---
+++ src/components/Manager.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Manager = () => {
+    return (
+        <div className="relative h-full w-full bg-slate-950">
+            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]">
+            </div>
+        </div>
+    )
+}
+
+export default Manager
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='bg-slate-700 text-white'>
+          <div className='container flex justify-between items-center px-4 py-5 h-14'>
+            
+          </div>    
+          
+    </nav>
+  )
+}
+
+export default Navbar
+
