6023c2d60e08185de37d5dd073f2960f958e3da8
unknown <satyamjhadar@gmail.com>
2024-09-11 07:53:16 +0530

    Table operations done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,19 @@ const Navbar = () => {
   return (
     <nav className='bg-slate-700 text-white'>
           <div className='container flex justify-between items-center px-4 py-5 h-14'>
+              <div className='logo text-white text-3xl font-bold'>
+                  <span className='text-blue-500'> &lt; </span>
+                  <span>Pass</span>
+                  <span className='text-blue-500'>OP/&gt;</span>
+              </div>
             
+              <button className='text-white bg-blue-600 my-5 mx-2 rounded-full flex justify-between items-center ring-1'>
+                  <img className='invert w-10 p-1' src="icons/github.png" alt="" />
+                <span></span>
+              </button>
           </div>    
-          
     </nav>
   )
 }
 
 export default Navbar
-
